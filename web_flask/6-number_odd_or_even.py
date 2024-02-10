fbd3a8b80be4845c4a5ce876e00c0c434e81f027
Onyedikachi Eze <thekacchi1@gmail.com>
2024-02-10 14:33:47 -0800

    pucodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,7 +46,10 @@ def number_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """Displays an HTML page with 'Number: n is even|odd' inside an H1 tag"""
-    return render_template('6-number_odd_or_even.html', n=n, even_odd='even' if n % 2 == 0 else 'odd')
+    return render_template(
+        '6-number_odd_or_even.html', n=n,
+        even_odd='even' if n % 2 == 0 else 'odd'
+    )
 
 
 if __name__ == '__main__':
