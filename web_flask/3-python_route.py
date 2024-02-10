9380f575b7dedf091c2925324169e26b092c96d0
Onyedikachi Eze <thekacchi1@gmail.com>
2024-02-09 18:42:13 -0800

    fixed pycode at ylr error

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_route(text):
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
 def python_route(text):
-    """Displays 'Python ', followed by the value of the text variable (default: 'is cool')"""
+    """Displays 'Python ', followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
